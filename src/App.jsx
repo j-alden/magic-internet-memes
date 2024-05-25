a55f595175c372e7d2066c5bea56aebd37fd8782
j-alden <jholt25@gmail.com>
2024-05-25 08:05:22 -0500

    Weirdness with header reverting

--- src/App.jsx
+++ src/App.jsx
@@ -351,16 +351,16 @@ const App = () => {
       >
         {/* <Container> */}
         <AppShell.Header
-          style={{
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            //height: rem(60),
-            zIndex: 1000000,
-            transform: `translate3d(0, ${pinned ? 0 : rem(-110)}, 0)`,
-            transition: 'transform 400ms ease',
-          }}
+        // style={{
+        //   position: 'fixed',
+        //   top: 0,
+        //   left: 0,
+        //   right: 0,
+        //   //height: rem(60),
+        //   zIndex: 1000000,
+        //   transform: `translate3d(0, ${pinned ? 0 : rem(-110)}, 0)`,
+        //   transition: 'transform 400ms ease',
+        // }}
         >
           <Stack h={'100%'} justify='center'>
             <Image src='/mim-banner.png' mah='100px' fit='contain' />
